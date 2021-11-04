c77239b62fe2a02ab5ade6839bed0172c3c74e95
Vivek Anand <vivek17212797@gmail.com>
2021-11-04 11:45:25 +0530

    changed venv name

--- ChemPlus1/settings.py
+++ ChemPlus1/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    '/home/ubuntu/env/lib/python3.8/site-packages/django/contrib/admin/static'
+    '/home/ubuntu/venv/lib/python3.8/site-packages/django/contrib/admin/static'
 ]
 #STATIC_ROOT = BASE_DIR + "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
