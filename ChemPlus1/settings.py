1b54cbbb9dcb1c2f3449edba209ffb1a29cdac44
Vivek Anand <vivek17212797@gmail.com>
2023-03-18 22:26:18 +0530

    req

--- ChemPlus1/settings.py
+++ ChemPlus1/settings.py
@@ -126,11 +126,11 @@ LOGIN_REDIRECT_URL = 'login'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'web/static'),
+    os.path.join(BASE_DIR, 'static'),
     '/home/ubuntu/venv/lib/python3.10/site-packages/django/contrib/admin/static'
 ]
 #STATIC_ROOT = BASE_DIR + "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
