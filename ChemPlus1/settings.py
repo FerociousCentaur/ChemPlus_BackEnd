fc54cafb191f45ca58680ba5b0ca8d66ef1741f1
Vivek Anand <vivek17212797@gmail.com>
2023-03-18 23:05:20 +0530

    req

--- ChemPlus1/settings.py
+++ ChemPlus1/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'ChemID',
     'localflavor',
 ]
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,8 +132,8 @@ STATICFILES_DIRS = [
     '/home/ubuntu/venv/lib/python3.10/site-packages/django/contrib/admin/static'
 ]
 #STATIC_ROOT = BASE_DIR + "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 
